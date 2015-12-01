433826933e77d5b581ca7194763954935be59565
SergeiDev <shemshur90@gmail.com>
2015-12-01 20:29:13 +0200

    adapters implemented

--- src/org/geekhub/json/adapters/DateAdapter.java
+++ src/org/geekhub/json/adapters/DateAdapter.java
@@ -1,14 +1,16 @@
 package org.geekhub.json.adapters;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * Converts object of type java.util.Date to String by using dd/MM/yyyy format
  */
 public class DateAdapter implements JsonDataAdapter<Date> {
+
+
     @Override
     public Object toJson(Date date) {
-        //implement me
-        return null;
+        return new SimpleDateFormat("dd/MM/yyyy").format(date);
     }
 }
