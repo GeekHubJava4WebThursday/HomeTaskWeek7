d541e82318103a9b851b5cc9e315e9d0a89a8067
Sergiy Govorukhin <govorukhin@gmail.com>
2015-12-03 14:41:38 +0200

    Implemented toJsonObject method

--- src/org/geekhub/json/JsonSerializer.java
+++ src/org/geekhub/json/JsonSerializer.java
@@ -1,10 +1,12 @@
 package org.geekhub.json;
 
+import org.geekhub.json.adapters.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+
+import java.awt.*;
+import java.lang.reflect.Field;
+import java.util.*;
 
 
 /**
@@ -68,9 +70,50 @@ public class JsonSerializer {
      * @return JSON object.
      * @throws IllegalAccessException
      * @throws InstantiationException
+     * @throws SecurityException
      */
     private static JSONObject toJsonObject(Object o) throws Exception {
-        //implement me
-        return null;
+        JSONObject jsonObject = new JSONObject();
+        Class clazz = o.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        // iterate all fields and process them
+        for (Field field: fields) {
+
+            // check if field must be ignored
+            if (field.isAnnotationPresent(Ignore.class)) {
+                continue;
+            }
+
+            // get field name and value
+            String name = null;
+            Object value = null;
+            try {
+                field.setAccessible(true);
+                name = field.getName();
+                value = field.get(o);
+            } catch (SecurityException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            if (field.isAnnotationPresent(UseDataAdapter.class)) {
+                UseDataAdapter annotation = field.getAnnotation(UseDataAdapter.class);
+                if (annotation.value().equals(ColorAdapter.class)) {
+                    jsonObject.put(name, serialize((new ColorAdapter()).toJson((Color) value)));
+                }
+                if (annotation.value().equals(DateAdapter.class)) {
+                    jsonObject.put(name, serialize((new DateAdapter()).toJson((Date) value)));
+                }
+                if (annotation.value().equals(MapAdapter.class)) {
+                    jsonObject.put(name, serialize((new MapAdapter()).toJson((Map) value)));
+                }
+                if (annotation.value().equals(CollectionAdapter.class)) {
+                    jsonObject.put(name, serialize((new CollectionAdapter()).toJson((Collection) value)));
+                }
+            } else {
+                jsonObject.put(name, serialize(value));
+            }
+        }
+        return jsonObject;
     }
 }
